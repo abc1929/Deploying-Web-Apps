5041f846bc3b02f6c8c629fb80efb39489e49c26
Jiawei Wu <abc1929@gmail.com>
2021-02-26 00:17:31 +0800

    test

---
+++ portfolio/pages/index.js
@@ -0,0 +1,16 @@
+// import Head from "next/head";
+
+export default function Home() {
+   return (
+      <div className="flex flex-col items-center">
+         <header>
+            <nav>
+               <h1> Jiawei Wu </h1>
+            </nav>
+         </header>
+         <div>
+            <button className="bg-green-100 px-6 py-2"> Test </button>
+         </div>
+      </div>
+   );
+}
